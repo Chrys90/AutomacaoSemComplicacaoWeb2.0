5011d46d1f500f6a5c759f5746fb7b6b2a77e825
Chrystopher Gois <chrystophergois@hotmail.com>
2024-01-15 13:56:47 -0300

    Aula 15 maps e page

---
+++ src/test/java/br/com/ChronosAcademy/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package br.com.ChronosAcademy.pages;
+
+import br.com.ChronosAcademy.core.Driver;
+import br.com.ChronosAcademy.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    LoginMap loginMap;
+
+    public LoginPage() {
+        loginMap = new LoginMap();
+        PageFactory.initElements(Driver.getDriver(), LoginMap);
+    }
+
+    public void clickBtnLogin(){
+        loginMap.btnlogin.click();
+    }
+
+    public void clickBtnFechar(){
+        loginMap.bntFechar.click();
+    }
+
+    public void clickDivFecharModal(){
+        loginMap.divFechaModal.click();
+    }
+
+    public void setInpUserName(String username){
+        loginMap.inpUserName.sendKeys(username);
+    }
+
+    public void setInpPassword(String password){
+        loginMap.inpPassword.sendKeys(password);
+    }
+
+    public void clickInpRemember(){
+        loginMap.inpRemember.click();
+    }
+
+    public void clickLinkCreateAccount(){
+        loginMap.linkCreateAccount.click();
+    }
+
+
+    public void clickBtnSignIn(){
+        loginMap.btnSignIn.click();
+    }
+
+    public boolean isBtnSignIn(){
+       return loginMap.btnSignIn.isEnabled();
+    }
+
+
+
+
+}
