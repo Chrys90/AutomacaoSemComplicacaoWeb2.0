190b2b6f5ea2a808a580f7dd14dc63a8dc425fb6
Chrystopher Gois <chrystophergois@hotmail.com>
2024-01-30 20:01:02 -0300

    aula 23 e 24 printscreen e headless

--- src/test/java/br/com/ChronosAcademy/steps/LoginSteps.java
+++ src/test/java/br/com/ChronosAcademy/steps/LoginSteps.java
@@ -6,6 +6,7 @@ import br.com.ChronosAcademy.pages.LoginPage;
 import br.com.ChronosAcademy.pages.NewAccountPage;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
@@ -14,6 +15,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -24,13 +26,19 @@ public class LoginSteps {
     String login;
 
     @Before
-    public void iniciaNavegador(){
+    public void iniciaNavegador(Scenario cenario) {
         new Driver(Browser.CHROME);
+        Driver.setNomeCenario(cenario.getName());
+        Driver.criaDiretorio();
     }
 
     @After
-    public void fechaNavegador(){
+    public void fechaNavegador(Scenario cenario) throws IOException {
+        if (cenario.isFailed()) {
+            Driver.printScreen("erro no cenario");
+        }
         Driver.getDriver().quit();
+
     }
 
     @Dado("que a modal esteja sendo exibida")
@@ -41,45 +49,53 @@ public class LoginSteps {
         loginPage.visibilityOfBtnFechar();
         loginPage.aguardaLoader();
     }
+
     @Quando("for realizado um clique no icone de fechar da modal")
-    public void forRealizadoUmCliqueNoIconeDeFecharDaModal() {
+    public void forRealizadoUmCliqueNoIconeDeFecharDaModal() throws IOException {
         loginPage.clickBtnFechar();
+        Driver.printScreen("clique fechar modal");
     }
+
     @Entao("a janela da modal deve ser fechada")
     public void aJanelaDaModalDeveSerFechada() throws Exception {
         try {
             loginPage.invisibilityOfBtnFechar();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new Exception("A janela modal não foi fechada");
         }
+        Driver.printScreen("modal fechada");
     }
 
 
     @Quando("for realizado um clique fora da modal")
-    public void forRealizadoUmCliqueForaDaModal() {
+    public void forRealizadoUmCliqueForaDaModal() throws IOException {
         loginPage.clickDivFecharModal();
+        Driver.printScreen("clique fora modal");
     }
 
     @Entao("a janela de modal deve ser fechada")
-    public void aJanelaDeModalDeveSerFechada() {
+    public void aJanelaDeModalDeveSerFechada() throws IOException {
         loginPage.clickBtnFechar();
+        Driver.printScreen("modal fechada");
     }
 
     @Quando("for realizado um clique no link do Create new Account")
-    public void forRealizadoUmCliqueNoLinkDoCreateNewAccount() {
+    public void forRealizadoUmCliqueNoLinkDoCreateNewAccount() throws IOException {
         loginPage.clickLinkCreateAccount();
+        Driver.printScreen("clique link create account");
     }
 
     @Entao("a pagina do create new account deve ser exibida")
-    public void aPaginaDoCreateNewAccountDeveSerExibida() {
+    public void aPaginaDoCreateNewAccountDeveSerExibida() throws IOException {
         NewAccountPage newAccountPage = new NewAccountPage();
         Assert.assertEquals("CREATE ACCOUNT", newAccountPage.getTextNewAccount());
         loginPage.aguardaLoader();
+        Driver.printScreen("tela create account");
     }
 
     @Quando("informar os campos de login e senha")
     public void informarOsCamposDeLoginESenha() {
-        
+
     }
 
     @Quando("clicar no botao sign in")
@@ -88,12 +104,13 @@ public class LoginSteps {
     }
 
     @Entao("deve ser possivel logar no sistema")
-    public void deveSerPossivelLogarNoSistema() {
+    public void deveSerPossivelLogarNoSistema() throws IOException {
         Assert.assertEquals(login, loginPage.getUsuarioLogado());
+        Driver.printScreen("logado no sistema");
     }
 
     @Quando("os campos de login sejam preenchidos da seguinte forma")
-    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma(Map<String, String> map) {
+    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma(Map<String, String> map) throws IOException {
 
         login = map.get("login");
         String senha = map.get("senha");
@@ -101,26 +118,29 @@ public class LoginSteps {
 
         loginPage.setInpUserName(login);
         loginPage.setInpPassword(senha);
-        if (remember)loginPage.clickInpRemember();
+        if (remember) loginPage.clickInpRemember();
 
+        Driver.printScreen("preenchimento dos campos de login");
     }
 
     @Quando("for realizado o clique no botao sign in")
     public void forRealizadoOCliqueNoBotaoSignIn() {
         loginPage.clickBtnSignIn();
+
     }
 
     @Entao("o sistema deve exibir uma mensagem de erro")
-    public void oSistemaDeveExibirUmaMensagemDeErro() {
+    public void oSistemaDeveExibirUmaMensagemDeErro() throws IOException {
         Assert.assertEquals("Incorrect user name or password.", loginPage.getErroLogin());
+        Driver.printScreen("login invalido");
     }
 
     @Entao("o botao sign in deve permanecer desabilitado")
-    public void oBotaoSignInDevePermanecerDesabilitado() {
+    public void oBotaoSignInDevePermanecerDesabilitado() throws IOException {
         boolean enabled = loginPage.isBtnSignIn();
         Assert.assertFalse(enabled);
+        Driver.printScreen("sign desabilitado");
     }
-
 
 
 }
